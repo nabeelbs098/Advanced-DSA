fa122d7a4f3ee327c0378da36953ecb627973923
Nabeel B S <nabeelbaiju5566@gmail.com>
2026-02-12 23:39:32 +0530

    object and ponter

---
+++ object_and_pointer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Student
+{public:
+    int id;
+    string name;
+    void display()
+    {
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+    }
+};
+int main()
+{
+    Student s1;
+    s1.id = 1;
+    s1.name = "Alice";
+    s1.display();
+
+    Student* ptr = &s1;
+    cout << "Using pointer to access members:" << endl;
+    cout << "ID: " << ptr->id << endl;
+    cout << "Name: " << ptr->name << endl;
+
+    return 0;
+}
